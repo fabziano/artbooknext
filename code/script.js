dba9066e7ec66bf5748aaac61721ba890fd82225
fabziano <fabianotmp@gmail.com>
2025-09-22 15:11:34 -0300

    code

--- code/script.js
+++ code/script.js
@@ -1,26 +1,55 @@
-import { obterCodigoGerado } from './gerar.js';
+let codigoGerado = null;
+let cepSequencial = 10000; // Valor inicial do CEP sequencial
 
-async function validarCodigo(codigoInputado, obterCodigoGerado) {
-    if (obterCodigoGerado === null) {
+async function buscarCodigoGerado() {
+    try {
+        const resposta = await fetch(`https://viacep.com.br/ws/01001000/json/`);
+        const dados = await resposta.json();
+        const cep = parseInt(dados.cep.replace('-', ''), 10); // Remove o hífen e converte para número
+        const aleatorio = Math.floor(Math.random() * 10000);
+        const codigo = (cep + cepSequencial + aleatorio) % 10000;
+        cepSequencial++; // Incrementa o CEP sequencial para o próximo valor
+        return codigo.toString().padStart(4, '0');
+    } catch (erro) {
+        console.error('Falha ao buscar o código gerado:', erro);
+        return null;
+    }
+}
+
+async function iniciarGeracaoPeriodica() {
+    codigoGerado = await buscarCodigoGerado();
+    console.log(codigoGerado);  // Agora o código será impresso após a geração inicial
+
+    setInterval(async () => {
+        codigoGerado = await buscarCodigoGerado();
+    }, 15000);
+}
+
+function obterCodigoGerado() {
+    return codigoGerado;
+}
+
+async function validarCodigo(codigoInputado) {
+    const codigoGerado = await obterCodigoGerado();
+    if (codigoGerado === null) {
         return false;
     }
-    return codigoInputado === obterCodigoGerado;
+    return codigoInputado === codigoGerado;
 }
+
 const input = document.querySelector('input');
 const button = document.querySelector('button');
 const p = document.querySelector('p');
 
 button.addEventListener('click', async () => {
     const codigoDigitado = input.value;
-    const isValido = await validarCodigo(codigoDigitado, obterCodigoGerado);
-    if (isValido) {
-        p.textContent = 'Válido';
-    } else {
-        p.textContent = 'Inválido';
-    }
+    const isValido = await validarCodigo(codigoDigitado);
+    p.textContent = isValido ? 'Válido' : 'Inválido';
 });
 
 input.addEventListener('input', (event) => {
     event.target.value = event.target.value.replace(/\D/g, '');
     p.textContent = '';
 });
+
+iniciarGeracaoPeriodica();
