874841e4ba245ba908e48f919127ca3b987038bd
fabziano <fabianotmp@gmail.com>
2025-09-22 18:36:05 -0300

    teste

--- code/script.js
+++ code/script.js
@@ -1,53 +1,17 @@
-let codigoGerado = null;
-const dataReferencia = new Date('2020-01-01T00:00:00Z'); // Data de referência fixa
-const intervaloSegundos = 15; // Intervalo de 15 segundos
+async function timestamp() {
+  const api = 'http://www.horalegalbrasil.mct.on.br/RelogioServidor.php';
+  const url = `https://corsproxy.io/?${encodeURIComponent(api)}`;
+  try {
 
-function gerarCodigo() {
-    // Obtém o número de segundos desde a data de referência fixa
-    const agora = Date.now();
-    const segundosDesdeReferencia = Math.floor((agora - dataReferencia.getTime()) / 1000);
-    
-    // Divide os segundos pelo intervalo (15 segundos)
-    const intervaloSequencial = Math.floor(segundosDesdeReferencia / intervaloSegundos);
+    const response = await fetch(url);
+    const html = await response.text();
 
-    // Gera o código baseado no intervalo sequencial (usar módulo 10000 para garantir 4 dígitos)
-    const codigo = intervaloSequencial % 10000;
-    
-    // Retorna o código com 4 dígitos, preenchendo com zero à esquerda se necessário
-    return codigo.toString().padStart(4, '0');
+    const conteudo = document.getElementById('id');
+    conteudo.innerHTML = html;
+
+  } catch (e) {
+    console.error(e);
+  }
 }
+timestamp();
 
-function obterCodigoGerado() {
-    if (codigoGerado === null) {
-        codigoGerado = gerarCodigo();
-    }
-    return codigoGerado;
-}
-
-async function validarCodigo(codigoInputado) {
-    const codigoGerado = obterCodigoGerado();
-    return codigoInputado === codigoGerado;
-}
-
-const input = document.querySelector('input');
-const button = document.querySelector('button');
-const p = document.querySelector('p');
-
-button.addEventListener('click', async () => {
-    const codigoDigitado = input.value;
-    const isValido = await validarCodigo(codigoDigitado);
-    p.textContent = isValido ? 'Válido' : 'Inválido';
-});
-
-input.addEventListener('input', (event) => {
-    event.target.value = event.target.value.replace(/\D/g, '');
-    p.textContent = '';
-});
-
-// Atualiza o código a cada 15 segundos
-setInterval(() => {
-    codigoGerado = gerarCodigo();
-    console.log(codigoGerado);  // Exibe o código gerado no console (a cada 15 segundos)
-}, 15000);
-
-console.log(obterCodigoGerado()); // Exibe o código gerado inicialmente no console
