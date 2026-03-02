d731935fed42f805f5d5694116dee497f23f9156
Fabiano <fabianotmp@gmail.com>
2026-03-01 21:46:26 -0300

    Add files via upload

---
+++ documents/layout.js
@@ -0,0 +1,17 @@
+class AppLayout extends HTMLElement {
+    connectedCallback() {
+        const currentPath = window.location.pathname.split('/').pop() || 'index.html';
+
+        document.body.insertAdjacentHTML('afterbegin', `
+            <header>
+                <nav>
+                    <a href="index.html" class="${currentPath === 'index.html' ? 'active' : ''}">Início</a>
+                    <a href="faculdade.html" class="${currentPath === 'faculdade.html' ? 'active' : ''}">Faculdade</a>
+                </nav>
+            </header>
+        `);
+        
+        this.remove();
+    }
+}
+customElements.define('layout', AppLayout);
